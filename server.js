72a5c2836d29c4f1a5cc828fb84e5456a69b1e49
h3nryc <henry.confos@student.tsc.nsw.edu.au>
2016-05-18 18:02:11 +1000

    Server change
    -Ports r bae

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(server);
 var url = require("url");
 var cookieParser = require('cookie-parser');
