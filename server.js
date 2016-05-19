4cb232b9dde076ca19e5781962b9544e8fcb718b
h3nryc <henry.confos@student.tsc.nsw.edu.au>
2016-05-19 17:24:03 +1000

    Open Shift
    -Changes to server ip

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 var io = require('socket.io').listen(server);
 var url = require("url");
 var cookieParser = require('cookie-parser');
